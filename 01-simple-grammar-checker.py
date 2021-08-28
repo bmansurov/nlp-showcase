93ecba5feac9a905973660bd6300a056a1ab7aaa
bmansurov <45986298+bmansurov@users.noreply.github.com>
2021-08-28 14:07:53 -0400

    Initial commit: add a simple grammar checker
    Given a list of similarly looking texts, find out which one is more
    grammatically correct.

---
+++ 01-simple-grammar-checker.py
@@ -0,0 +1,35 @@
+"""Given a list of similarly looking texts, find out which one is more
+grammatically correct.
+
+"""
+
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+def loss(text):
+    inputs = tokenizer(text, return_tensors="pt")
+    outputs = model(**inputs, labels=inputs["input_ids"])
+    return outputs.loss.cpu().detach().numpy()
+
+def get_most_likely_text(texts):
+    losses = [loss(text) for text in texts]
+    return(texts[losses.index(min(losses))])
+
+
+texts = ['Somebody call policeman.',
+         'Somebody call a policeman.']
+print(get_most_likely_text(texts))
+
+texts = ['I went home.',
+         'I went to home.']
+print(get_most_likely_text(texts))
+
+texts = ["It's a beautiful town.",
+         "Its a beautiful town."]
+print(get_most_likely_text(texts))
+
+texts = ["I have lived here for three weeks.",
+         "I have lived here since three weeks."]
+print(get_most_likely_text(texts))
